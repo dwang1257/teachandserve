e379a5ee65b9855b816772a390214096091e7007
Dylan Wang <97680620+dwang1257@users.noreply.github.com>
2025-06-16 20:47:47 -0400

    first commit:

---
+++ backend/src/main/java/com/teachandserve/backend/TeachAndServeApplication.java
@@ -0,0 +1,13 @@
+package com.teachandserve.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachAndServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachAndServeApplication.class, args);
+	}
+
+}
