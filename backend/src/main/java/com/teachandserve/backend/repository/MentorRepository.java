65f17e20397323e0536d6bbdb984d98fd1c16926
Eddie <edd1e20786@gmail.com>
2025-07-13 20:57:13 +0800

    mentor repository created

--- backend/src/main/java/com/teachandserve/backend/repository/MentorRepository.java
+++ backend/src/main/java/com/teachandserve/backend/repository/MentorRepository.java
@@ -1,5 +1,9 @@
 package com.teachandserve.backend.repository;
 
-public class MentorRepository {
-    
+import com.teachandserve.backend.model.Mentor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
 }
